54fee929e8a1bff635639a5316fbe7b915291259
Maryem El Ajleni <ajlanimeryam@gmail.com>
2020-06-05 12:20:11 +0200

    RegistrationFixe

--- project/src/Form/UserType.php
+++ project/src/Form/UserType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +18,59 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('First_Name')
-            ->add('Last_Name')
-            ->add('Email')
-            ->add('Password')
-            ->add('Phone_Number')
-            ->add('Profile_Picture')
-            ->add('User_Name')
-            ->add('address')
+            ->add('First_Name',TextType::class , array(
+                'attr' => array(
+                    'class' => 'form-control',
+                    'Placeholder'=>'First Name')))
+
+            ->add('Last_Name',TextType::class,array(
+                'attr'=>array(
+                    'class' => 'form-control',
+                    'Placeholder'=>'Last Name'
+                )
+            ))
+            ->add('User_Name',TextType::class,array(
+                'attr'=>array(
+                    'class' => 'form-control adjust',
+                    'Placeholder'=>'UserName'
+                )
+            ))
+            ->add('Email',EmailType::class,array(
+                'attr'=>array(
+                    'class' => 'form-control',
+                    'Placeholder'=>'Email'
+                )
+            ))
+            ->add('Password', RepeatedType::class, [
+                'type' =>PasswordType::class,
+                'first_options'  => array(
+                    'attr'=>array(
+                        'class' => 'form-control',
+                        'Placeholder'=>'Password'
+                    ),
+                ),
+                'second_options' => array(
+                    'attr'=>array(
+                        'class' => 'form-control',
+                        'Placeholder'=>'Password Check'
+                    )
+                ),
+                'invalid_message' => 'Passwords Do Not Match',
+                'second_name'=>'confirm',
+                'first_name'=>'password'
+            ])
+
+            ->add('Phone_Number',IntegerType::class,array(
+                'attr'=>array(
+                    'class' => 'form-control adjust',
+                    'Placeholder'=>'Phone Number'
+                )
+            ))
+            ->add('Register',SubmitType::class,array(
+                'attr'=>array(
+                    'class' => 'btn btn-primary',
+                )
+            ))
         ;
     }
 
