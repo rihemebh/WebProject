23313a5e126d1cab36af351a62c8ffb64ac50890
Maryem El Ajleni <ajlanimeryam@gmail.com>
2020-06-05 11:32:57 +0200

    RegistrationFixed

---
+++ project/src/Form/UserType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('First_Name')
+            ->add('Last_Name')
+            ->add('Email')
+            ->add('Password')
+            ->add('Phone_Number')
+            ->add('Profile_Picture')
+            ->add('User_Name')
+            ->add('address')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
